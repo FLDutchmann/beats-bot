6204d8f94d9aae35c15574f501e211277f9b76b3
Arend Mellendijk <arend.mellendijk@gmail.com>
2021-09-18 20:12:32 +0200

    Added Queue Command

--- commands/queue.js
+++ commands/queue.js
@@ -3,16 +3,23 @@ const { getVoiceConnection, createAudioResource } = require('@discordjs/voice');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('skip')
-		.setDescription('Skip the current song'),
+		.setName('queue')
+		.setDescription('List the queued songs'),
 	async execute(interaction) {
 		const channel = interaction.member.voice.channel;
 		let connection = getVoiceConnection(channel.guild.id);
         if(!connection) {
+            await interaction.reply('Not currently playing');
             return;
         }
-        const player = connection.player;
-        player.stop();
-        await interaction.reply(`Skipping Song`);
+        
+        let str = "";
+
+        for(let i = 0; i < 10; i++){
+            if(i >= connection.queue.length) break;
+            str += `${i+1}) ${connection.queue[i]} \n`
+        }
+
+        await interaction.reply(str);
 	},
 };
